4ef12cd79266ffbcb301da8852b9da559cb25162
Adeliu4239 <132083364+Adeliu4239@users.noreply.github.com>
2023-04-28 23:28:17 +0800

    Add files via upload

---
+++ HW2_個人自介網站old/js/nav.js
@@ -0,0 +1,21 @@
+
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+    isNavbarExpanded = !isNavbarExpanded;
+    navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+function scrollToSection(id) {
+    var section = document.getElementById(id);
+    var sectionTop = section.getBoundingClientRect().top + window.pageYOffset - 72;
+    window.scrollTo({ top: sectionTop, behavior: 'smooth' });
+}
